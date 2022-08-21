5bbc25d7a8b7ac92353cb4342b317871046a297e
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-08-21 20:48:23 +0430

    config mongodb schema & model

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const routes = express.Router();
 
+const Workout = require("../models/workout-model");
+
 routes.get("/", (req, res) => {
   res.json({ mssg: "GET all workouts" });
 });
@@ -9,8 +11,18 @@ routes.get("/", (req, res) => {
 routes.get("/:id", (req, res) => {
   res.json({ mssg: "GET a single workout" });
 });
-routes.post("/", (req, res) => {
-  res.json({ mssg: "POST a workout" });
+
+// POST a new workout
+routes.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ mssg: error.message });
+  }
+  // res.json({ mssg: "POST a workout" });
 });
 routes.delete("/:id", (req, res) => {
   res.json({ mssg: "DELETE a workout" });
